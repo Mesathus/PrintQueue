56941f9358b422321fa1ec482a7dce71ee960a5a
John O'Brien <mesathus@bellsouth.net>
2018-09-24 23:30:04 -0400

    Queue monitoring functions added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,24 +29,50 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 101);
+            this.button1.Location = new System.Drawing.Point(41, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 82);
+            this.button1.Size = new System.Drawing.Size(63, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Resume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(906, 601);
+            this.ClientSize = new System.Drawing.Size(453, 313);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,20 +6,81 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
 using System.Windows.Forms;
 
 namespace PrintQueue
 {
     public partial class Form1 : Form
     {
+        private static readonly Timer printTimer = new Timer();
+
         public Form1()
         {
             InitializeComponent();
+            printTimer.Tick += CheckJobs;
+            printTimer.Interval = 5000;
+            printTimer.Start();
         }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            GetPrintJob();
+        }
+
+        private static void CheckJobs(object myobject, EventArgs e)
+        {
+            printTimer.Stop();
+            string searchQuery = "SELECT * FROM Win32_PrintJob";
+            ManagementObjectSearcher printJobs = new ManagementObjectSearcher(searchQuery);
+            ManagementObjectCollection printJobCollection = printJobs.Get();
+            if(printJobCollection.Count > 0)
+            {
+                foreach(ManagementObject job in printJobCollection)
+                {
+                    if(int.TryParse(job["TotalPages"].ToString(), out int pages))
+                    {
+                        if(pages > 1)
+                        {
+                            job.Delete();
+                        }
+                        else
+                        {
+                            MessageBox.Show(job["TotalPages"].ToString() + " page to print");
+                        }
+                    }
+                }
+                MessageBox.Show("Jobs in Queue");
+            }
+            printTimer.Start();
+        }
+
+        private static void GetPrintJob()
+        {
+            string searchQuery = "SELECT * FROM Win32_PrintJob";
+            ManagementObjectSearcher printJobs = new ManagementObjectSearcher(searchQuery);
+            ManagementObjectCollection printJobCollection = printJobs.Get();
+            foreach(ManagementObject job in printJobCollection)
+            {
+                MessageBox.Show(job["Name"].ToString() + 
+                                "\n" + job["Owner"].ToString() +
+                                "\n" + job["TimeSubmitted"].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //pause
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //resume
+
         }
     }
 }
