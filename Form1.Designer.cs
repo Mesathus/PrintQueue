c454517137a1f06566d458c696541c96535d5d80
Mesathus <mesathus@bellsouth.net>
2018-09-25 18:55:36 -0400

    GUI listboxes added, implemetation testing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,14 +31,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pqListBox = new System.Windows.Forms.ListBox();
+            this.totalListBox = new System.Windows.Forms.ListBox();
+            this.resetUserButton = new System.Windows.Forms.Button();
+            this.testButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 53);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(82, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 43);
+            this.button1.Size = new System.Drawing.Size(126, 83);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +50,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(195, 202);
+            this.button2.Location = new System.Drawing.Point(82, 403);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(150, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "Pause";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,23 +61,66 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(195, 278);
+            this.button3.Location = new System.Drawing.Point(82, 506);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(150, 44);
             this.button3.TabIndex = 2;
             this.button3.Text = "Resume";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // pqListBox
+            // 
+            this.pqListBox.FormattingEnabled = true;
+            this.pqListBox.ItemHeight = 25;
+            this.pqListBox.Location = new System.Drawing.Point(265, 102);
+            this.pqListBox.Name = "pqListBox";
+            this.pqListBox.Size = new System.Drawing.Size(732, 254);
+            this.pqListBox.TabIndex = 3;
+            // 
+            // totalListBox
+            // 
+            this.totalListBox.FormattingEnabled = true;
+            this.totalListBox.ItemHeight = 25;
+            this.totalListBox.Location = new System.Drawing.Point(1125, 102);
+            this.totalListBox.Name = "totalListBox";
+            this.totalListBox.Size = new System.Drawing.Size(376, 454);
+            this.totalListBox.TabIndex = 4;
+            // 
+            // resetUserButton
+            // 
+            this.resetUserButton.Location = new System.Drawing.Point(1125, 615);
+            this.resetUserButton.Name = "resetUserButton";
+            this.resetUserButton.Size = new System.Drawing.Size(132, 87);
+            this.resetUserButton.TabIndex = 5;
+            this.resetUserButton.Text = "Reset User";
+            this.resetUserButton.UseVisualStyleBackColor = true;
+            this.resetUserButton.Click += new System.EventHandler(this.resetUserButton_Click);
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(1125, 820);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(132, 120);
+            this.testButton.TabIndex = 6;
+            this.testButton.Text = "Test Features";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 313);
+            this.ClientSize = new System.Drawing.Size(1636, 991);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.resetUserButton);
+            this.Controls.Add(this.totalListBox);
+            this.Controls.Add(this.pqListBox);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -84,6 +132,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox pqListBox;
+        private System.Windows.Forms.ListBox totalListBox;
+        private System.Windows.Forms.Button resetUserButton;
+        private System.Windows.Forms.Button testButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,9 @@ namespace PrintQueue
     public partial class Form1 : Form
     {
         private static readonly Timer printTimer = new Timer();
+        //Print queue network path:   \\ALABSPRINTSRV01\A-208 HP M605 Pool
+        //ManagementObject a208printer = new ManagementObject("\\\\ALABSPRINTSRV01\\A-208 HP M605 Pool");
+        
 
         public Form1()
         {
@@ -22,9 +25,6 @@ namespace PrintQueue
             printTimer.Interval = 5000;
             printTimer.Start();
         }
-
-
-
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -43,7 +43,7 @@ namespace PrintQueue
                 {
                     if(int.TryParse(job["TotalPages"].ToString(), out int pages))
                     {
-                        if(pages > 1)
+                        if (pages > 2)
                         {
                             job.Delete();
                         }
@@ -81,6 +81,46 @@ namespace PrintQueue
         {
             //resume
 
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            List<string> loginNames = new List<string>();
+            loginNames.Clear();
+            totalListBox.Items.Clear();
+            loginNames.Add("A208-01\t\t15\t5");
+            loginNames.Add("A208-02\t\t12\t8");
+            loginNames.Add("A208-03\t\t17\t3");
+            loginNames.Add("A208-04\t\t3\t2");
+            totalListBox.Items.Add("Location\t\tDaily\tCurrent");
+            foreach(string item in loginNames)
+            {
+                totalListBox.Items.Add(item);
+            }
+        }
+
+        private void resetUserButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(totalListBox.SelectedIndex != -1 && totalListBox.SelectedIndex > 0)
+                {
+                    int index = totalListBox.SelectedIndex;
+                    string user = totalListBox.SelectedItem.ToString();
+                    string[] userInfo = user.Split('\t');
+                    userInfo[3] = "0";
+                    totalListBox.Items.RemoveAt(index);
+                    totalListBox.Items.Insert(index, userInfo[0] + "\t\t" + userInfo[2] + "\t" + userInfo[3]);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a user to reset current page count for.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error resetting user page count.");
+            }
         }
     }
 }
